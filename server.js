449b92a91e5a525a2cbbef0c05e4c57a1654ecf0
ThatJohn <fajardocj@gmail.com>
2020-01-30 05:37:56 -0500

    Adds basic routes.

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
